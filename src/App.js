3467ea14c694b538debe38e055ccdc561382a4af
Kip Schetnan <kipdougschetnan@gmail.com>
2023-01-28 18:37:31 -0600

    added App.js and index.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
