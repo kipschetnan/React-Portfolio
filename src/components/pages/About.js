75048d6311939d1330ea1b673be3ce8c99f11ffd
Kip Schetnan <kipdougschetnan@gmail.com>
2023-02-13 18:08:16 -0600

    added to my about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -19,16 +19,11 @@ function About() {
                 <FontAwesomeIcon icon={faCircleUser} />
             </div>
             <p className='p'>
-                This is placement text for now. This is placement text for now. 
-                This is placement text for now. This is placement text for now. 
-                This is placement text for now. This is placement text for now. 
-                This is placement text for now. This is placement text for now. 
-                This is placement text for now. This is placement text for now. 
-                This is placement text for now. This is placement text for now. 
-                This is placement text for now. This is placement text for now. 
-                This is placement text for now. This is placement text for now. 
-                This is placement text for now. This is placement text for now. 
-                This is placement text for now. This is placement text for now. 
+                I am a graduate from the Full Stack Web Development Bootcamp with the University of Minnesota - St. Paul. 
+                Hoping to start my career in web development and to continue learning and developing my skills as a developer.
+                I have experience in many languages that include, HTML, JavaScript, CSS, Python, Java, C++. Always striving to be 
+                the best at whatever I do and to be the best person I could possibly be. I believe I have a strong work ethic 
+                to the point where I feel I need to put in the necessary work to reach expectations and more.
             </p>
         </div>
     )
